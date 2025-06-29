6e1608fa0e8315fe7913c25c6fc3c7a66b6ae478
Matobro <Harri.taberman@hotmail.com>
2025-06-29 05:25:14 +0300

    Clean up directories

---
+++ Commands/Scripts/CommandsData.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name CommandsData
+
+var command_object = preload("res://Commands/Command.tscn")
+var move_command = preload("res://Commands/Types/move_command.tres")
+var rally_point = preload("res://Commands/Types/rally_point.tres")
+var attack_move_command = preload("res://Commands/Types/attack_move_command.tres")
+var attack_move_rally = preload ("res://Commands/Types/attack_move_rally.tres")
