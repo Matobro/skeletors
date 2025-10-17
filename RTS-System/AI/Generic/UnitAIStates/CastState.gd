43fbf108976eb4b00a537fa671db9b62a52ab2d1
Matobro <Harri.taberman@hotmail.com>
2025-10-17 23:12:48 +0300

    added 3-phase casting
    animations are synced to the actual casting.
    first plays casting-start, then loops casting-loop and in the end of cast plays casting-end

--- RTS-System/AI/Generic/UnitAIStates/CastState.gd
+++ RTS-System/AI/Generic/UnitAIStates/CastState.gd
@@ -4,7 +4,7 @@ var ability_to_cast: BaseAbility
 var ability_to_cast_index: int
 var ability_target: Unit
 var arrived: bool = false
-
+var is_casting: bool = false
 var target_position
 var target_unit
 
@@ -14,7 +14,7 @@ signal cast_cancelled()
 
 func enter_state():
 	print("Entered casting state")
-
+	is_casting = false
 	command = ai.get_current_command()
 	target_position = command["target_position"]
 	target_unit = command["target_unit"]
@@ -47,19 +47,49 @@ func state_logic(delta):
 
 func on_arrival():
 	var ability = ability_to_cast
+
 	if(!arrived):
 		print("Arrived at casting location")
+
+		#Connect signal for ability
 		ability.connect("cast_done", Callable(self, "on_cast_finished"), CONNECT_ONE_SHOT)
 		arrived = true
-		var anim_speed = ai.animation_player.get_animation_speed("casting") / ability.ability_data.cast_time
-		ai.animation_player.play_animation("casting", anim_speed)
+
+		#Start 3-phase animation
+		start_casting_animation(ability.ability_data.cast_time)
+
+		#Start casting ability
 		parent.unit_ability_manager.cast_ability(command["context"])
+
+func start_casting_animation(cast_time: float):
+	is_casting = true
+
+	#Play casting-start
+	print("Playing casting-start")
+	ai.animation_player.play_animation("casting-start", 1)
+	await ai.animation_player.animation_finished
+	if !is_casting: return
+
+	#Play casting-loop
+	print("Playing casting-loop")
+	ai.animation_player.play_animation("casting-loop", 1)
+	var buffer = ai.animation_player.get_animation_speed("casting-loop")
+	await ai.get_tree().create_timer(max(0.0, cast_time - buffer)).timeout
+	if !is_casting: return
+
+	#Play casting-end
+	print("Playing casting-end")
+	ai.animation_player.play_animation("casting-end", 1)
+	await ai.animation_player.animation_finished
+	if is_casting:
+		on_cast_finished()
 
 func clear_state():
 	var ability = ability_to_cast
 	if ability_to_cast != null and ability.is_connected("cast_done", Callable(self, "on_cast_finished")):
 		ability.disconnect("cast_done", Callable(self, "on_cast_finished"))
 
+	is_casting = false
 	ability_to_cast = null
 	ability_to_cast_index = -1
 	ability_target = null
